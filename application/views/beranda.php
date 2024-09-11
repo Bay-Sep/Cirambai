e3a87eca5dc738df36a0e21c2b485eecd588d33a
Bay-Sep <bayysep01@gmail.com>
2024-09-11 18:50:53 +0700

    third commit

---
+++ application/views/beranda.php
@@ -0,0 +1,50 @@
+<div class="content-wrapper">
+    <section class="content">
+        <div class="box box-info">
+            <div class="box-header">
+                <h4 style="text-align:center"><b>SELAMAT DATANG DI SISTEM INFORMASI PENGELOLAAN DATA PENDUDUK<br>KANTOR
+                        KEPALA DESA RAMBAI</b></h4>
+                <hr>
+            </div>
+            <div style="text-align: center;"> <img style="cent"
+                    src="<?php echo base_url('assets/images/logo-pariaman.png'); ?>" width="200"
+                    style="margin-right:50px"> </div>
+
+
+            <br>
+            <br>
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h4 style="text-align:center"> <b> VISI DAN MISI </b></h4>
+                            <hr>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div style="text-align: left;" class="col-sm-5 col-sm-offset-1">
+                            <p>Melaksanakan Program-Program Pemerintah Desa Rambai Yang Efisien, Efektif Dan Bersih, Mengembangkan Perekonomian Desa Secara Kreatif, Meningkatkan Keimanan Dan Ketaqwaan, Serta Terciptanya Suasana Yang Aman, Rukun Dan Berkualitas</p>
+                        </div>
+
+                        <div style="text-align: left;" class="col-sm-5 col-sm-offset-1">
+                            <p> 1.Optimasi Penyelengaraan Pemerintah Desa Rambai Yang Meliputi : <br>
+                                     - Penyelengaraan Pemerintahan Yang Transparan Dan Akuntabel <br>
+                                     - Pelayanan Terhadap Masyarakat Yang Ramah, Tepat, Benar Dan Bersih <br>
+                                     - Melaksanakan Pembangunan Yang Berkesinambungan Dan Mengedepankan Kepentingan Bersama <br>
+                                2.Memberdayakan Semua Potensi Yang Ada Di Masyarakat Yang Meliputi : <br>
+                                     - Pemberdayaan Sumber Daya Manusia (SDM) <br>
+                                     - Pemberdayaan Sumber Daya Manusia (SDM) <br>
+                                     - Meningkatkan Perekonomian Masyarakat Melalui Pendampingan Berupa Penyusunan Khusus UKM, Wiraswasta Dan Petani <br>
+                                3.Menanamkan Rasa Taat Dengan Mengaktifkan Program Magrib Mengaji Kembali Kesuarau, Mengadakan Wirid Bulanan Dimasjid, Didikan Subuh Dan Mengimbau Anak-Anak Kita Untuk Meramaikan MDA Yang Ada Di Desa Rambai <br>
+                                4.Menciptakan Kondisi Masyarakat Desa Rambai Yang Aman, Tertib Dan Rukun Dakam Kehidupan Bermasyarakat Dengan Berpegang Pada Prinsip “Duduak Samo Randah Tagak Samo Tinggi, Ringan Samo Dijijiang Barek Samo Dipikua”
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            </center>
+        </div>
+    </section>
+</div>
